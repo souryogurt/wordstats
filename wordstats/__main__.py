17ee78e1ec4e2717128febcac2c6bfa399942e39
Egor Artemov <egor.artemov@gmail.com>
2019-07-20 00:08:29 +0300

    Print histogram of line lengths

--- wordstats/__main__.py
+++ wordstats/__main__.py
@@ -3,6 +3,7 @@
 
 import argparse
 import fileinput
+import shutil
 import sys
 
 from . import __version__
@@ -10,6 +11,22 @@ from . import __version__
 __all__ = ['main']
 
 VERSION = 'wordstats {}'.format(__version__)
+
+
+def print_histogram(length, lines_number, max_length, max_number, max_width):
+    """Prints line length, number of lines of such length, and histogram."""
+    lwidth = len(str(max_length))
+    nwidth = len(str(max_number))
+    max_width = max_width - lwidth - nwidth - 2
+    item = {
+        'length': length,
+        'lwidth': lwidth,
+        'num': lines_number,
+        'nwidth': nwidth,
+        'histogram': '#' * int(max_width * lines_number / max_number),
+
+    }
+    print('{length:<{lwidth}} {num:<{nwidth}} {histogram}'.format(**item))
 
 
 def main():
@@ -21,8 +38,19 @@ def main():
 
     args = parser.parse_args()
     files = args.files if args.files else ('-',)
+
+    lengths = dict()
     for line in fileinput.input(files=files):
-        print(line.strip())
+        line_length = len(line.strip())
+        lengths[line_length] = lengths.get(line_length, 0) + 1
+    min_length = min(lengths.keys())
+    max_length = max(lengths.keys())
+    max_number = max(lengths.values())
+    term_size = shutil.get_terminal_size((80, 24))
+    max_width = term_size.columns
+    for length in range(min_length, max_length + 1):
+        num = lengths.get(length, 0)
+        print_histogram(length, num, max_length, max_number, max_width)
 
 
 if __name__ == "__main__":
